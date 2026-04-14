83c7b64f2f02dd49619b73d33700d0bbe9405bf2
Veera-B <veerabashipangu@gmail.com>
2026-04-14 18:53:23 +0530

    nx docker azur setup

---
+++ apps/micro-frontends/auth-portal/src/app/app.jsx
@@ -0,0 +1,11 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@org/auth-portal" />
+    </div>
+  );
+}
+export default App;
