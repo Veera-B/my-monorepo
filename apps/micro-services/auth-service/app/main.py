fb33c4e889ca75d8972efcff445f545a96ebc84d
Veera-B <veerabashipangu@gmail.com>
2026-05-06 11:47:11 +0530

    path based/ conditional filter ci-cd flow

--- apps/micro-services/auth-service/app/main.py
+++ apps/micro-services/auth-service/app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to Azure! FastAPI is working fine."}
+    return {"message": "Welcome to Azure! FastAPI is working fine Dude!"}
